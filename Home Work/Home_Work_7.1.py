ee22267c3dd191232ac844b52035f45e885d2fc7
RolanKUS <Rolankusainov@icloud.com>
2026-07-08 17:31:28 +0300

    home_work_7.1

---
+++ Home Work/Home_Work_7.1.py
@@ -0,0 +1,6 @@
+###
+def say_hi(name,age):
+    return f"Hello my name is {name} and I am {age} years old"
+assert say_hi("Rolan",27) == "Hello my name is Rolan and I am 27 years old"
+print("OK")
+print(say_hi("Rolan",27))
